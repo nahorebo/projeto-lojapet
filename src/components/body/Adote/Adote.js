7fd5eb8553db2c3db53d745276936bc19bfb439a
nahorebo <vicnomi11@gmail.com>
2022-11-30 23:34:21 -0300

    subindo o headernav

--- src/components/body/Adote/Adote.js
+++ src/components/body/Adote/Adote.js
@@ -3,16 +3,16 @@ import "./styles.css";
 
 const Adote = () => {
     return(
-<div id="#Adote">
-<div>
-  <img className="img-adoto"
+<div className="containeradote">
+<div className="divcontaineradote">
+  <img className="imgadote"
     src=""
     alt="Cachorrinho deitado"
     width="500"
     height="600">
   </img>
   </div>
-  <div>
+  <div className="">
     <h1>Ainda não tem um bichinho?</h1>
   <p> Uma seleção especial de peludinhos que buscam um lar para chamar de seu</p>
   <p> visite nossa loja e conheça outros pets disponíveis para adoção pertinho de você.</p>
--- src/components/headernav/Headernav.js
+++ src/components/headernav/Headernav.js
@@ -1,35 +1,70 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import Imgheader from "../assets/caeegato.jpg";
+import logo from "../assets/logoprincipal.jpeg";
 import "./styles.css"
 
 function Headernav() {
   return (
-    <header>
+    // <header>
 
+    // <div className="logo2">
+    //   <h2 className="nome">Pet Codigos</h2>
+
+    //   <nav className="menu">
+    //     <ul>
+    //       <Link to="/">Sobre</Link>
+    //       <Link to="/post">Produtos</Link>
+    //       <Link to="/Catalogo">Catálogo</Link>
+    //       <Link to="/Adote">Adote</Link>
+    //       <Link to="/Contato">Contato</Link>
+    //     </ul>
+    //   </nav>
+
+    //   <div className="logo1">
+    // <img
+    //         className="imgcontainer"
+    //           src={Imgheader}
+    //           alt="Cachorro marrom"
+    //           width="300" height="400"/>
+    //         <h2 className="nome">Pet Codigos</h2>
+    //     </div>
+    // </div>
+    // </header>
+
+    <nav className="navbar">
     <div className="logo">
-      <h2 className="nome">Pet Codigos</h2>
-
-      <nav className="menu">
-        <ul>
-          <Link to="/">Sobre</Link>
-          <Link to="/post">Produtos</Link>
-          <Link to="/Catalogo">Catálogo</Link>
-          <Link to="/Adote">Adote</Link>
-          <Link to="/Contato">Contato</Link>
-        </ul>
-      </nav>
-
-      <div className="logo">
-    <img
-            className="imgcontainer"
-              src={Imgheader}
-              alt="Cachorro marrom"
-              width="300" height="400"/>
-            <h2 className="nome">Pet Codigos</h2>
-        </div>
+        <Link to= "/"> <img className="logo1"
+    //     {/* <h2>Pet Codigos </h2> */}
+    // {/* <img /> */}
+    src={logo}
+    alt="Logo de Pet Codigos"
+    width={250}
+    height={60}>
+    </img>
+    </Link>
     </div>
-    </header>
+
+    {/* <div class="nav-menu">
+    <input type="checkbox" id="check"/>
+    <label for="check" class="checkbtn">
+        <i class="menu-icon"><img src={menu} alt=""/></i>
+    </label> */}
+
+
+    <div className="menu-lista">
+        {/* <ul>
+        <li> */}
+    <Link to="/" className="nav-link">Produtos</Link>
+    <Link to="/post" className="nav-link">Catálogo</Link>
+    <Link to="/" className="nav-link">Adote</Link>
+    <Link to="/" className="nav-link">Contato</Link>                
+    {/* </li>
+    </ul> */}
+    </div>
+    {/* </div> */}
+</nav>
+
+
 
 
   );
